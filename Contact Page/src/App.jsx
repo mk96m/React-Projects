3659555a83dfbf6e610b395b21c0f6a951d04413
Mayukh Mandal <139850822+mk96m@users.noreply.github.com>
2023-12-18 12:42:19 +0530

    Add files via upload

---
+++ Contact Page/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navigation />
+        <Contact />
+      </div>
+    </>
+  )
+}
+
+export default App;

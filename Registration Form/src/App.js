21e79cd1e1c8c1a66353d915a4e5b3b3a829e414
Mayukh Mandal <139850822+mk96m@users.noreply.github.com>
2023-12-18 13:08:15 +0530

    Add files via upload

---
+++ Registration Form/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import Register from "./Components/Register";
+import Greet from "./Components/Greet";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isRegistered: false,
+      name: null,
+      email: null,
+      password: null,
+      showPass: false,
+    };
+  }
+
+  registrationHandler = (event) => {
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    this.setState({ name, email, password, isRegistered: true });
+    event.preventDefault();
+  };
+
+  showPasswordHandler = () => {
+    this.setState({ showPass: !this.state.showPass });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isRegistered ?
+          (<Greet name={this.state.name} email={this.state.email} ></Greet>)
+          : (<Register
+            submit={this.registrationHandler}
+            showPass={this.state.showPass}
+            click={this.showPasswordHandler}
+          ></Register>
+          )}
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
